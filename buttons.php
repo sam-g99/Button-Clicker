59b7b983870a184d8b5b6878f613734f3de8b019
sam-g99 <46730010+sam-g99@users.noreply.github.com>
2020-01-18 10:33:13 -0500

    button click

--- buttons.php
+++ buttons.php
@@ -6,6 +6,40 @@ if (!isset($_SESSION['username'])) {
     header('Location: /login.php');
     exit();
 }
+
+require 'config.php';
+
+function jsonRes($status, $msg)
+{
+    return json_encode(array(
+        'status' => $status,
+        'msg' => $msg
+    ));
+}
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+    $button = trim($_POST['button']);
+
+    if (empty($button)) {
+        echo  jsonRes(400, "No button set.");
+        exit();
+    }
+
+    if ($button !== "blue button" && $button !== "green button" && $button !== "gray button") {
+        echo jsonRes(400, "button is invalid");
+        exit();
+    }
+
+    $sql = "INSERT INTO Clicks (button, username) VALUES (?, ?)";
+
+    $stmt = $pdo->prepare($sql);
+
+    $stmt->execute([$button, $_SESSION['username']]);
+
+    echo jsonRes(201, "Click registered");
+    exit();
+}
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -25,13 +59,29 @@ if (!isset($_SESSION['username'])) {
     ?>
     <div class="center">
         <div class="buttons-container">
-            <button id="blueButton">Click Me</button>
-            <button id="greenButton">Click Me</button>
-            <button id="grayButton">Click Me</button>
+            <button id="blueButton" onclick="buttonClick('blue button')">Click Me</button>
+            <button id="greenButton" onclick="buttonClick('green button')">Click Me</button>
+            <button id="grayButton" onclick="buttonClick('gray button')">Click Me</button>
         </div>
 
         <a href="/stats.php" class="view-stats"> View Stats </a>
     </div>
+
+    <script>
+        const buttonClick = async (button) => {
+            const formData = new FormData();
+
+            formData.append("button", button);
+
+            const response = await fetch('/buttons.php', {
+                method: 'POST',
+                body: formData,
+            })
+
+            const data = await response.text();
+            console.log(data);
+        }
+    </script>
 </body>
 
 </html>
