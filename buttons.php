01e44dc755fb7abea4e99e8a79b8c5b690345893
sam-g99 <46730010+sam-g99@users.noreply.github.com>
2020-01-18 13:29:56 -0500

    button page title

--- buttons.php
+++ buttons.php
@@ -49,7 +49,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="/styles/main.css">
-    <title>Document</title>
+    <title>Buttons</title>
 </head>
 
 <body>
